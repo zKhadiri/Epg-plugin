31e83d14a61e1282f700fb8ade3e59041fd1c753
RAED <rrrr53@hotmail.com>
2020-04-09 19:09:02 +0300

    Update installer.sh

--- Download/installer.sh
+++ Download/installer.sh
@@ -2,7 +2,7 @@
 ##setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/ziko-ZR1/Epg-plugin/master/Download/installer.sh -O - | /bin/sh
 
 ######### Only These two lines to edit with new version ######
-version=3.5
+version=3.6
 description=What is NEW:\n[new update]
 ##############################################################
 # remove old version
