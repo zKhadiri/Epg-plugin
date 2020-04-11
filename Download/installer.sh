512b5316ffd6793b939e60046de6f20f659be42e
RAED <rrrr53@hotmail.com>
2020-04-11 03:58:47 +0300

    Update installer.sh

--- Download/installer.sh
+++ Download/installer.sh
@@ -9,43 +9,51 @@ description=What is NEW:\n[new update]
 rm -rf /usr/lib/enigma2/python/Plugins/Extensions/Epg_Plugin
 
 # check depends packges
-if [ -f /var/lib/dpkg/status ]; then
-   STATUS=/var/lib/dpkg/status
-   OS = 'DreamOS'
-else
-   STATUS=/var/lib/opkg/status
-   OS = 'Opensource'
+if [ -f /etc/apt/apt.conf ] ; then
+   STATUS='/var/lib/dpkg/status'
+   OS='DreamOS'
+elif [ -f /etc/opkg/opkg.conf ] ; then
+   STATUS='/var/lib/opkg/status'
+   OS='Opensource'
 fi
-if grep -q python-requests $STATUS; then
-    requests=Installed
+if grep -q 'python-requests' $STATUS; then
+    requests='Installed'
 fi
-if grep -q enigma2-plugin-extensions-epgimport $STATUS; then
-    epgimport=Installed
+if grep -q 'enigma2-plugin-extensions-epgimport' $STATUS; then
+    epgimport='Installed'
 fi
 if [ $requests = "Installed" -a $epgimport = "Installed" ]; then 
      echo ""
 else
      echo "Need to download Depends packages"
-     opkg update
-     if grep -q python-requests $STATUS; then
+     if [ $OS = "DreamOS" ]; then
+          apt-get update
+     else
+          opkg update
+     fi
+     if grep -q 'python-requests' $STATUS; then
           echo ""
      else
           if [ $OS = "DreamOS" ]; then 
+                  echo " Downloading python-requests ......"
                   apt-get install python-requests -y
           else
+                  echo " Downloading python-requests ......"
                   opkg install python-requests
           fi
      fi
-     if grep -q python-requests $STATUS; then
+     if grep -q 'enigma2-plugin-extensions-epgimport' $STATUS; then
           echo ""
      else
           if [ $OS = "DreamOS" ]; then
+                   echo " Downloading epgimport ......"
                    cd /tmp
                    wget https://github.com/ziko-ZR1/Epg-plugin/blob/master/Download/enigma2-plugin-extensions-epgimport_1.0-r200-all.deb
                    dpkg -i *.deb
                    apt-get install -f -y
                    cd ..
           else
+                   echo " Downloading epgimport ......"
                    opkg install enigma2-plugin-extensions-epgimport
           fi
      fi
