a9b414f3b814fe71c8cad6fe08fa3da0f8ef776c
RAED <rrrr53@hotmail.com>
2020-04-11 04:36:03 +0300

    Update installer.sh

--- Download/installer.sh
+++ Download/installer.sh
@@ -49,9 +49,9 @@ else
                    echo " Downloading epgimport ......"
                    cd /tmp
                    wget -q "--no-check-certificate" "https://github.com/ziko-ZR1/Epg-plugin/blob/master/Download/enigma2-plugin-extensions-epgimport_1.0-r200-all.deb";
-                   dpkg -i *.deb;
-                   apt-get install -f -y;
                    cd ..
+                   dpkg -i /tmp/*.deb;
+                   apt-get install -f -y;
           else
                    echo " Downloading epgimport ......"
                    opkg install enigma2-plugin-extensions-epgimport
