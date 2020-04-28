d152e7da745644f45d2b09519d696617578a3156
ziko-ZR1 <zakariya-khadiri@hotmail.fr>
2020-04-28 21:04:49 +0000

    bein fix

--- Epg_Plugin/scripts/bein.py
+++ Epg_Plugin/scripts/bein.py
@@ -66,11 +66,12 @@ def bein():
                 titles.append(4*' '+'<title lang="en">'+title_.replace('&','and')+' - '+form_.replace('2014','2020')+'</title>'+'\n')
 
             for time_,chann_ in zip(times,channels):
+                from datetime import timedelta
                 date = re.search(r'\d{4}-\d{2}-\d{2}',url)
                 if time_[1].encode('ascii')=='00:00':
-                    fix = int(datetime.strptime(date.group(),'%Y-%m-%d').strftime('%d')) + 1
+                    fix = (datetime.strptime(date.group(),'%Y-%m-%d')+timedelta(days=1)).strftime('%Y-%m-%d')
                     starttime = datetime.strptime(date.group()+' '+time_[0],'%Y-%m-%d %H:%M').strftime('%Y%m%d%H%M%S')
-                    endtime = datetime.strptime(now+'-'+str(fix) + ' ' + time_[1], '%Y %m-%d %H:%M').strftime('%Y%m%d%H%M%S')
+                    endtime = datetime.strptime(fix+' ' + time_[1], '%Y-%m-%d %H:%M').strftime('%Y%m%d%H%M%S')
                     prog.append(2 * ' ' + '<programme start="' + starttime + ' '+time_zone+'" stop="' + endtime + ' '+time_zone+'" channel="'+chann_.replace('BS NBA','BS_NBA')+'">'+'\n')
                 else:
                     starttime = datetime.strptime(date.group()+' '+time_[0],'%Y-%m-%d %H:%M').strftime('%Y%m%d%H%M%S')
