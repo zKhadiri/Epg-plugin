eb07e21a2e73217feebf89bd2273f0e28ec16df4
fairbird <rrrr53@hotmail.com>
2020-05-07 15:53:27 +0300

    Bump V6.5 - Add new option to download providers EPG

--- Download/installer.sh
+++ Download/installer.sh
@@ -2,8 +2,8 @@
 ##setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/ziko-ZR1/Epg-plugin/master/Download/installer.sh -O - | /bin/sh
 
 ######### Only These two lines to edit with new version ######
-version=6.4
-description=What_is_NEW:\n'[SNRT FIX]'
+version=6.5
+description=What_is_NEW:\n'[Change download feature option]'
 ##############################################################
 # No need to remove old version
 #rm -rf /usr/lib/enigma2/python/Plugins/Extensions/Epg_Plugin
