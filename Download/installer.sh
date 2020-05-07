b6dc5b67c7a04ef9328eacb3d4d5c35ec20aa0a7
ziko-ZR1 <zakariya-khadiri@hotmail.fr>
2020-05-07 18:36:21 +0000

    bug

--- Download/installer.sh
+++ Download/installer.sh
@@ -2,7 +2,7 @@
 ##setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/ziko-ZR1/Epg-plugin/master/Download/installer.sh -O - | /bin/sh
 
 ######### Only These two lines to edit with new version ######
-version=6.7
+version=6.6
 description=What_is_NEW:\n'[new source : spacetoon and mbc egy]'
 ##############################################################
 # No need to remove old version
