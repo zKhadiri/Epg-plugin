48d0e63248763debc785207ac170daec7f06eb71
ziko-ZR1 <zakariya-khadiri@hotmail.fr>
2020-05-18 21:30:56 +0000

    new update

--- Epg_Plugin/scripts/osnos.py
+++ Epg_Plugin/scripts/osnos.py
@@ -84,6 +84,13 @@ def oss():
     with io.open("/etc/epgimport/osn.xml", "a",encoding="utf-8") as f:
         f.write(('</tv>').decode('utf-8'))
         
+    from datetime import datetime
+    with open("/usr/lib/enigma2/python/Plugins/Extensions/Epg_Plugin/times/osn.txt") as f:
+        lines = f.readlines()
+    lines[1] = datetime.today().strftime('%A %d %B %Y at %I:%M %p')
+    with open("/usr/lib/enigma2/python/Plugins/Extensions/Epg_Plugin/times/osn.txt", "w") as f:
+        f.writelines(lines)
+        
 def progressbar(it, prefix="", size=20, file=sys.stdout):
     count = len(it)
     def show(j):
@@ -100,12 +107,7 @@ def progressbar(it, prefix="", size=20, file=sys.stdout):
   
 if __name__=='__main__':
     oss()
-    from datetime import datetime
-    with open("/usr/lib/enigma2/python/Plugins/Extensions/Epg_Plugin/times/osn.txt") as f:
-        lines = f.readlines()
-    lines[1] = datetime.today().strftime('%A %d %B %Y at %I:%M %p')
-    with open("/usr/lib/enigma2/python/Plugins/Extensions/Epg_Plugin/times/osn.txt", "w") as f:
-        f.writelines(lines)
+    
     
 
 
