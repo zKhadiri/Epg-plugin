cf7955d151756c349356686e5387368f3850101d
RAED <rrrr53@hotmail.com>
2020-05-30 22:35:02 +0300

    Black to old version of epgimport1.0-r203 for DreamOS

--- Download/installer.sh
+++ Download/installer.sh
@@ -55,7 +55,7 @@ else
           if [ $OS = "DreamOS" ]; then
                    echo " Downloading/Insallling epgimport ......"
                    sleep 3
-                   wget -q "--no-check-certificate" "https://github.com/ziko-ZR1/Epg-plugin/blob/master/Download/enigma2-plugin-extensions-epgimport_1.8-r219_all.deb?raw=true" -O "/tmp/enigma2-plugin-extensions-epgimport_1.8-r219_all.deb";
+                   wget -q "--no-check-certificate" "https://github.com/ziko-ZR1/Epg-plugin/blob/master/Download/enigma2-plugin-extensions-epgimport_1.0-r203-all.deb?raw=true" -O "/tmp/enigma2-plugin-extensions-epgimport_1.0-r203-all.deb";
                    dpkg -i /tmp/*.deb;
                    apt-get install -f -y;
           else
