168acc935d9e8f7b9230059d60dd79de91df9d09
ziko-ZR1 <43075184+ziko-ZR1@users.noreply.github.com>
2020-06-28 22:32:47 +0100

    Update installer.sh

--- Download/installer.sh
+++ Download/installer.sh
@@ -2,7 +2,7 @@
 ##setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/ziko-ZR1/Epg-plugin/master/Download/installer.sh -O - | /bin/sh
 
 ######### Only These two lines to edit with new version ######
-version=10.7
+version=10.8
 description=What_is_NEW:\n'[NEW SOURCE : SETANTA]'
 ##############################################################
 
