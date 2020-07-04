f24e138e8bc403cd16f2317bc3db096f4366dc9c
ziko-ZR1 <43075184+ziko-ZR1@users.noreply.github.com>
2020-07-04 17:40:13 +0100

    Update configs.py

--- Epg_Plugin/scripts/configs.py
+++ Epg_Plugin/scripts/configs.py
@@ -3,7 +3,7 @@ import requests,sys,io
 print('Downloading custom.sources config')
 sys.stdout.flush()
 custom_channels=requests.get('https://github.com/ziko-ZR1/Epg-plugin/blob/master/configs/custom.sources.xml?raw=true')
-with io.open('/etc/epgimport/custom.channels.xml','w',encoding="utf-8") as f:
+with io.open('/etc/epgimport/custom.sources.xml','w',encoding="utf-8") as f:
     f.write(custom_channels.text)
         
 print('Downloading latest channels list')
