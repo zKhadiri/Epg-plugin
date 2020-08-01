110df049b96031acac88e37a64b23e2198d5d42f
ziko-ZR1 <43075184+ziko-ZR1@users.noreply.github.com>
2020-08-01 13:00:38 +0000

    Update installer.sh

--- Download/installer.sh
+++ Download/installer.sh
@@ -2,8 +2,8 @@
 ##setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/ziko-ZR1/Epg-plugin/master/Download/installer.sh -O - | /bin/sh
 
 ######### Only These two lines to edit with new version ######
-version=12.0
-description=What_is_NEW:\n'[new source : sky italia epg]'
+version=12.1
+description=What_is_NEW:\n'[el cinema new channels]'
 ##############################################################
 
 TEMPATH=/tmp
