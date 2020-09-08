68eba1eecec5d2070840d3a6031e7084069a02c8
ziko-ZR1 <zakariya-khadiri@hotmail.fr>
2020-09-08 12:38:13 +0100

    discovery source update

--- Epg_Plugin/scripts/discovery.py
+++ Epg_Plugin/scripts/discovery.py
@@ -6,7 +6,7 @@ from requests.adapters import HTTPAdapter
 next_update = (datetime.today()+timedelta(days=20)).strftime('%Y-%m-%d 02:00:00')
 
 channels = ['Discovery Central Europe|DCENENG-UTC','Animal Planet Europe|APEUENG-UTC','Discovery Showcase HD|HDEUENG-UTC'
-            ,'Discovery Science Europe|SCEUENG-UTC']
+            ,'Discovery Science Europe|SCEUENG-UTC','ID|IDDEENG-UTC','DTX|TUTUROM-UTC']
 
 with io.open("/etc/epgimport/discovery.xml","w",encoding='UTF-8')as f:
     f.write(('<?xml version="1.0" encoding="UTF-8"?>'+"\n"+'<tv generator-info-name="By ZR1">').decode('utf-8'))
@@ -25,16 +25,24 @@ def discovery():
             date_start = re.findall(r'<BROADCAST_START_DATETIME>(.*?)</BROADCAST_START_DATETIME>',url.text)
             date_end = re.findall(r'<BROADCAST_END_TIME>(.*?)</BROADCAST_END_TIME>',url.text)
             description = re.findall(r'<TEXT_TEXT>(.*?)</TEXT_TEXT>',url.text)
-
-            for title,start,end,des in zip(titles,date_start,date_end,description):
+            dtx_titles = re.findall(r'<PROGRAMME_TITLE_ORIGINAL>(.*?)</PROGRAMME_TITLE_ORIGINAL>',url.text)
+            dtx_des = re.findall(r'<PROGRAMME_SUBTITLE_ORIGINAL>(.*?)</PROGRAMME_SUBTITLE_ORIGINAL>',url.text)
+            episode = re.findall(r'<EPISODE_NUMBER>(.*?)</EPISODE_NUMBER>',url.text)
+            programme_year = re.findall(r'<PROGRAMME_YEAR>(.*?)</PROGRAMME_YEAR>',url.text)
+            for title,start,end,des,ep,py,dtx_t,dxt_d in zip(titles,date_start,date_end,description,episode,programme_year,dtx_titles,dtx_des):
                 if end <=next_update:
                     last_date = end
                     prog_start = datetime.strptime(start,'%Y-%m-%d %H:%M').strftime('%Y%m%d%H%M%S')
                     prog_end = datetime.strptime(end,'%Y-%m-%d %H:%M').strftime('%Y%m%d%H%M%S')
                     epg=''
-                    epg+=2 * ' ' + '<programme start="' + prog_start + ' +0000" stop="' + prog_end + ' +0000" channel="'+ch.split('|')[0]+'">\n'
-                    epg+=4*' '+'<title lang="en">'+title.replace('&','and').replace('amp;','')+'</title>\n'
-                    epg+=4*' '+'<desc lang="en">'+des.replace('&','and').replace('amp;','').strip()+'</desc>\n  </programme>\r'
+                    if ch.split('|')[0]=="DTX":
+                        epg+=2 * ' ' + '<programme start="' + prog_start + ' +0000" stop="' + prog_end + ' +0000" channel="'+ch.split('|')[0]+'">\n'
+                        epg+=4*' '+'<title lang="en">'+dtx_t.replace('&amp;','and')+'</title>\n'
+                        epg+=4*' '+'<desc lang="en">Episode '+ep+' . '+py+' : '+dxt_d.replace('&amp;','and').strip()+'</desc>\n  </programme>\r'
+                    else:
+                        epg+=2 * ' ' + '<programme start="' + prog_start + ' +0000" stop="' + prog_end + ' +0000" channel="'+ch.split('|')[0]+'">\n'
+                        epg+=4*' '+'<title lang="en">'+title.replace('&amp;','and')+'</title>\n'
+                        epg+=4*' '+'<desc lang="en">Episode '+ep+' . '+py+' : '+des.replace('&amp;','and').strip()+'</desc>\n  </programme>\r'
                     with io.open("/etc/epgimport/discovery.xml","a",encoding='UTF-8')as f:
                         f.write(epg)
                     
