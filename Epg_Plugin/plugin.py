b3da44fe33cfb61cdf685ecbdfee03caa0015679
ziko-ZR1 <zakariya-khadiri@hotmail.fr>
2020-09-15 12:10:57 +0100

    add plugin to extensions menu

--- Epg_Plugin/plugin.py
+++ Epg_Plugin/plugin.py
@@ -1,10 +1,10 @@
 from Plugins.Plugin import PluginDescriptor
-import interfaces
+from interfaces import EPGIConfig
 from Screens.MessageBox import MessageBox
 import requests
 
 
-def connected_to_internet(): ## to test connection	
+def connected_to_internet():	
     try:
         _ = requests.get('https://github.com', timeout=5)
         return True
@@ -12,16 +12,14 @@ def connected_to_internet(): ## to test connection
         return False
 
 def main(session, **kwargs):
-    if connected_to_internet()==True:
-        session.open(interfaces.EPGIConfig)
+    if connected_to_internet():
+        session.open(EPGIConfig)
     else:
         session.open(MessageBox,_("No internet connection available. Or github.com Down"), MessageBox.TYPE_INFO,timeout=10)
   
 def Plugins(**kwargs):
-    return PluginDescriptor(
-        name="EPG GRABBER",
-        description="EPG WEB GRABBER BY ZIKO",
-        where = PluginDescriptor.WHERE_PLUGINMENU,
-        icon="epg.png",
-        fnc=main)
+    Descriptors=[]
+    Descriptors.append(PluginDescriptor(name="EPG GRABBER",description="EPG WEB GRABBER BY ZIKO",where = PluginDescriptor.WHERE_PLUGINMENU,icon="epg.png",fnc=main))
+    Descriptors.append(PluginDescriptor(name="EPG GRABBER",where = PluginDescriptor.WHERE_EXTENSIONSMENU,fnc=main))
+    return Descriptors
 
