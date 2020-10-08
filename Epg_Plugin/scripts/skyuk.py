c9f06a0abec66d6f1312d3f3aba47a15f89694c0
ziko-ZR1 <zakariya-khadiri@hotmail.fr>
2020-10-08 12:06:16 +0100

    uk sports epg fix

--- Epg_Plugin/scripts/skyuk.py
+++ Epg_Plugin/scripts/skyuk.py
@@ -16,7 +16,7 @@ time_zone=get_tz()
 
 uk_sports=['4010-SkySp PL HD',"3939-SkySp F'ball HD","4049-SkySp News HD",'4002-SkySpMainEvHD',
 '3625-BT Sport 1 HD','3627-BT Sport 2 HD','3629-BT Sport 3 HD','4040-BTSpt//ESPNHD','1218-BTSBoxOffWWE',
-'4081-SkySpCricketHD','3835-SkySp F1 HD','4022-SkySp ActionHD','3940-SkySp ArenaHD',
+'4081-SkySpCricketHD','3835-SkySp F1 HD','4022-SkySp NFL HD','3940-SkySp ArenaHD',
 '4026-SkySp Golf HD','4032-SkySp Racing HD','4090-SkySp Mix HD','1035-SkySpBoxOffHD','1015-LaLigaTV HD','5153-Premier 1 HD',
 '1634-Premier 2 HD','1150-FreeSports HD','1003-MUTV HD']
 
@@ -35,25 +35,31 @@ for i in range(0,7):
         urls.append('https://epgservices.sky.com/5.1.1/api/2.0/channel/json/'+code.split('-')[0]+'/'+str(int(days))+'/86395/4|'+code.split('-')[1])
 urls.sort()
 
-for link in urls:
-    with requests.Session() as s:
-        s.mount('http://', HTTPAdapter(max_retries=10))
-        url = s.get(link.split('|')[0]).json()
-        channel_code = re.findall(r'json/(\d+)/',link)
-        for data in url['listings'][''.join(channel_code)]:
-            start = datetime.fromtimestamp(data['s']).strftime('%Y%m%d%H%M%S')
-            end = (datetime.strptime(start,'%Y%m%d%H%M%S') + timedelta(seconds=data['m'][1])).strftime('%Y%m%d%H%M%S')
-            epg=''
-            epg+=2 * ' ' + '<programme start="' + start + ' '+time_zone+'" stop="' + end + ' '+time_zone+'" channel="'+link.split('|')[1]+'">\n'
-            epg+=4*' '+'<title lang="en">'+data['t'].replace('&','and')+'</title>\n'
-            epg+=4*' '+'<desc lang="en">'+data['d'].replace('&','and')+'</desc>\n  </programme>\r'
-            with io.open("/etc/epgimport/skyuk.xml","a",encoding='UTF-8')as f:
-                f.write(epg)
-    print(link.split('|')[1]+' epg date : '+(datetime.strptime(end,'%Y%m%d%H%M%S').strftime('%Y-%m-%d %H:%M')))
-    sys.stdout.flush()
-    
-with io.open("/etc/epgimport/skyuk.xml", "a",encoding="utf-8") as f:
-    f.write(('</tv>').decode('utf-8'))
+def sky():
+    for link in urls:
+        try:
+            with requests.Session() as s:
+                s.mount('http://', HTTPAdapter(max_retries=10))
+                url = s.get(link.split('|')[0]).json()
+                channel_code = re.findall(r'json/(\d+)/',link)
+                for data in url['listings'][''.join(channel_code)]:
+                    start = datetime.fromtimestamp(data['s']).strftime('%Y%m%d%H%M%S')
+                    end = (datetime.strptime(start,'%Y%m%d%H%M%S') + timedelta(seconds=data['m'][1])).strftime('%Y%m%d%H%M%S')
+                    epg=''
+                    epg+=2 * ' ' + '<programme start="' + start + ' '+time_zone+'" stop="' + end + ' '+time_zone+'" channel="'+link.split('|')[1]+'">\n'
+                    epg+=4*' '+'<title lang="en">'+data['t'].replace('&','and')+'</title>\n'
+                    epg+=4*' '+'<desc lang="en">'+data['d'].replace('&','and')+'</desc>\n  </programme>\r'
+                    with io.open("/etc/epgimport/skyuk.xml","a",encoding='UTF-8')as f:
+                        f.write(epg)
+            print(link.split('|')[1]+' epg date : '+(datetime.strptime(end,'%Y%m%d%H%M%S').strftime('%Y-%m-%d %H:%M')))
+            sys.stdout.flush()
+        except:continue
+
+
+if __name__ == "__main__":
+    sky()   
+    with io.open("/etc/epgimport/skyuk.xml", "a",encoding="utf-8") as f:
+        f.write(('</tv>').decode('utf-8'))
     
 
 with open('/usr/lib/enigma2/python/Plugins/Extensions/Epg_Plugin/times.json', 'r') as f:
