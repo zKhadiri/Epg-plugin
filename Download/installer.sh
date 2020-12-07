ea3d20604f4f96131befcba22ec4b296d783b7cc
RAED <rrrr53@hotmail.com>
2020-12-07 16:51:02 +0300

    Fix download bouquets.json - V 16.4

--- Download/installer.sh
+++ Download/installer.sh
@@ -2,8 +2,8 @@
 ##setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/ziko-ZR1/Epg-plugin/master/Download/installer.sh -O - | /bin/sh
 
 ######### Only These two lines to edit with new version ######
-version=16.3
-description=What_is_NEW:\n'[Fix bouquets.json.]'
+version=16.4
+description=What_is_NEW:\n'[Fix download bouquets.json.]'
 ##############################################################
 
 TEMPATH=/tmp
--- src/EPGGrabber/core/configs.py
+++ src/EPGGrabber/core/configs.py
@@ -15,6 +15,6 @@ with io.open('/etc/epgimport/custom.sources.xml','w',encoding="utf-8") as f:
         
 print('Downloading latest channels list')
 sys.stdout.flush()
-chnnales_lists=requests.get('https://github.com/ziko-ZR1/Epg-plugin/blob/master/Epg_Plugin/bouquets.json?raw=true')
+chnnales_lists=requests.get('https://raw.githubusercontent.com/ziko-ZR1/Epg-plugin/master/src/EPGGrabber/api/bouquets.json')
 with io.open(BOUQUETS_ROOT,'w',encoding="utf-8") as f:
     f.write(chnnales_lists.text)
