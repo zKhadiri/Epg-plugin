f12ace2bd303aea3fba01d6165af80f80c9fdf5b
ziko-ZR1 <43075184+ziko-ZR1@users.noreply.github.com>
2021-03-01 19:09:23 +0100

    Update installer.sh

--- Download/installer.sh
+++ Download/installer.sh
@@ -2,8 +2,8 @@
 ##setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/ziko-ZR1/Epg-plugin/master/Download/installer.sh -O - | /bin/sh
 
 ######### Only These two lines to edit with new version ######
-version=17.5
-description=What_is_NEW:\n'[ID EXTRA DOWNLOAD FIX.]'
+version=17.6
+description=What_is_NEW:\n'[add new beIN SPORT channels ]'
 ##############################################################
 
 TEMPATH=/tmp
