1f0545e10bffb0bfcfe01f3a61ea7c49c4905b76
RAED <rrrr53@hotmail.com>
2021-06-07 21:54:05 +0300

    Bump V18.5

--- Download/installer.sh
+++ Download/installer.sh
@@ -2,8 +2,8 @@
 ##setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/ziko-ZR1/Epg-plugin/master/Download/installer.sh -O - | /bin/sh
 
 ######### Only These two lines to edit with new version ######
-version=18.4
-description=What_is_NEW:\n'[Fix rotana & new backup sources for jawwy]'
+version=18.5
+description=What_is_NEW:\n'[Update url bein entertainment & add id for channel bein Sport FR 1,2,3]'
 ##############################################################
 
 TEMPATH=/tmp
