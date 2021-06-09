3b001b8c14035386a332ed514a4198faeff22789
MOHAMED_OS <47327706+MOHAMED19OS@users.noreply.github.com>
2021-06-09 11:28:49 +0100

    Update List Channel

--- src/EPGGrabber/providers/elcin.py
+++ src/EPGGrabber/providers/elcin.py
@@ -18,21 +18,19 @@ headers={
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36'
 }
 
-nb_channel=["1353-2M Monde","1312-Al Aoula International",'1101-Aloula', '1120-ART_Aflam_1', '1121-ART_Aflam_2', '1122-ART_Hekayat', 
-            '1134-ONDrama', '1135-Emirates', '1136-Abu_Dhabi_TV', '1137-Alhayat_TV', 
-            '1138-AlhayatDrama', '1145-Mehwar', '1148-RotanaCinema EGY', '1156-NileDrama', 
-            '1157-Nile_Cinema', '1158-NileComedy', '1159-NileLife', '1168-LBCI', '1169-Dubai_TV', 
-            '1170-AlraiTV', '1173-Dubai_One', '1174-AlKaheraWalNasTV', '1176-Cima', '1177-SamaDubai', 
-            '1177-Sama_Dubai', '1178-Abu_Dhabi_Drama', '1179-Dream', '1182-ART_Hekayat_2', 
-            '1188-SharjahTV', '1193-Al_Nahar_TV', '1195-ART_Cinema', '1198-CBC', '1199-CBC_Drama', 
-            '1203-ONE', '1204-iFILMTV', '1216-AlJadeedTV', '1217-Rotana_Classic', '1223-Al_Nahar_Drama', 
-            '1226-SadaElBalad', '1227-SadaElBaladDrama', '1252-AlKaheraWalNasTV2', '1260-CBC_sofra', 
-            '1261-Zee_alwan', '1262-Zee_aflam', '1264-AlDafrah', '1269-Alsharqya', '1279-SadaElBalad+2', 
-            '1280-TeNTV', '1283-Dubai_Zaman', '1290-DMC', '1292-DMC_DRAMA', '1296-MTVLebnon', '1297-SBC', 
-            '1298-Amman', '1299-Roya', '1300-SyriaDrama', '1301-Alsumaria', '1302-Fujairah', '1304-Nessma', 
-            '1308-Watania1', '1310-Kuwait', '1313-Lana', '1314-JordanTV', '1317-Oman', '1321-almanar', 
-            '1334-Watania2', '1336-MasperoZaman', '1338-SyriaTV', '1339-AlSaeedah', 
-            '1339-Al_Saeedah', '1341-LBC', '1342-LanaPlusTV', '1350-SamaTV', '1352-Saudiya_TV', '1355-Mix','1366-Thikrayat']
+nb_channel=['1173-DubaiOne', '1223-Al_NaharDrama', '1169-Dubai_TV', '1137-Alhayat_TV', '1199-CBC_Drama', '1176-Cima',
+            '1178-AbuDhabi_Drama', '1227-Sada_ElBalad_Drama', '1198-CBC', '1177-Sama_Dubai', '1193-Al_NaharTV', '1246-LDC',
+            '1174-Al_Kahera_Wal_NasTV', '1216-Al_JadeedTV', '1158-Nile_Comedy', '1135-Emirates', '1170-Alrai_TV', '1159-Nile_Life',
+            '1226-Sada_El_Balad', '1157-Nile_Cinema', '1168-LBCI','1179-Dream', '1252-Al_Kahera_Wal_NasTV2', '1264-Al_Dafrah',
+            '1203-ONE', '1101-Aloula', '1134-ONDrama', '1283-Dubai_Zaman', '1188-Sharjah_TV', '1241-MBC_3',
+            '1279-Sada_El_Balad2', '1313-Lana', '1269-AlSharqiya', '1242-Network_Arabic', '1280-TeNTV', '1298-Amman',
+            '1156-Nile_Drama', '1292-DMC_DRAMA', '1136-AbuDhabi_TV', '1297-SBC', '1301-Alsumaria', '1300-Syria_Drama',
+            '1299-Roya', '1138-Alhayat_Drama', '1310-Kuwait', '1304-Nessma', '1336-Maspero_Zaman', '1145-Mehwar',
+            '1302-Fujairah', '1308-Watania1', '1204-iFILM_TV', '1306-Alrasheed', '1312-Al_Aoula_Morocco', '1321-almanar',
+            '1338-Syria_TV', '1342-Lana_PlusTV', '1339-AlSaeedah', '1341-LBC', '1303-ATTESSIA', '1343-Musawa',
+            '1314-Jordan_TV', '1366-Thikrayat_Tv', '1353-2MTV', '1352-Saudiya_TV' ,'1349-IMN', '1334-Watania2',
+            '1360-SalamTV', '1367-Utv', '1368-BahrainTV', '1355-Mix', '1350-SamaTV', '1369-Qurain',
+            '1296-MTV', '1317-Oman', '1260-CBC_sofra', '1290-DMC', '1370-Khallik_Bilbait' ]
 
 
     
