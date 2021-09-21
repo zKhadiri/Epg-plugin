45c19029b80a36c4aa4a1383ae2c4946bd680fb5
ziko-ZR1 <43075184+ziko-ZR1@users.noreply.github.com>
2021-09-21 11:54:10 +0100

    Update installer.sh

--- Download/installer.sh
+++ Download/installer.sh
@@ -2,8 +2,8 @@
 ##setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/ziko-ZR1/Epg-plugin/master/Download/installer.sh -O - | /bin/sh
 
 ######### Only These two lines to edit with new version ######
-version=19.4
-description=What_is_NEW:\n'New sources : Canal+Afrique/elcinema english'
+version=19.5
+description=What_is_NEW:\n'Fix Canal+Afrique'
 ##############################################################
 
 TEMPATH=/tmp
