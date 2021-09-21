56410f57b3eb17015fe0c4b0560d2e03a3910949
ziko-ZR1 <43075184+ziko-ZR1@users.noreply.github.com>
2021-09-21 11:53:10 +0100

    Update caf.py

--- src/EPGGrabber/providers/caf.py
+++ src/EPGGrabber/providers/caf.py
@@ -12,22 +12,25 @@ def cplus():
     for code in ch_code:
         for i in range(0,8):
             url = requests.get('https://service.canal-overseas.com/ott-frontend/vector/83001/channel/'+code+'/events?filter.day='+str(i)).json()
-            for data in url['timeSlices']:
-                for prog in data['contents']:
-                    subtitle = 'information non disponible'
-                    if 'subtitle' in prog:
-                        subtitle = prog['subtitle']
-                    title,ch_name,startime,endtime = prog['title'],prog['thirdTitle'],prog['startTime'],prog['endTime']
-                    start = datetime.fromtimestamp(startime).strftime('%Y%m%d%H%M%S')
-                    end = datetime.fromtimestamp(endtime).strftime('%Y%m%d%H%M%S')
-                    ch = ''
-                    ch += 2 * ' ' + '<programme start="' + str(start) + ' '+time_zone+'" stop="' + str(end) + ' '+time_zone+'" channel="' + ch_name.replace('.','') + '">\n'
-                    ch += 4 * ' ' + '<title lang="fr">' + title.replace('&', 'and') + '</title>\n'
-                    ch += 4 * ' ' + '<desc lang="fr">' + subtitle.replace('&', 'and') + '</desc>\n  </programme>\r'
-                    with io.open(EPG_ROOT + '/caf.xml', "a", encoding='UTF-8')as f:
-                        f.write(ch)
-        print(ch_name.replace('.','')+' EPG ends at : '+str(datetime.fromtimestamp(endtime).strftime('%Y-%m-%d %H:%M')))
-        sys.stdout.flush()
+            try:
+                for data in url['timeSlices']:
+                    for prog in data['contents']:
+                        subtitle = 'information non disponible'
+                        if 'subtitle' in prog:
+                            subtitle = prog['subtitle']
+                        title,ch_name,startime,endtime = prog['title'],prog['thirdTitle'],prog['startTime'],prog['endTime']
+                        start = datetime.fromtimestamp(startime).strftime('%Y%m%d%H%M%S')
+                        end = datetime.fromtimestamp(endtime).strftime('%Y%m%d%H%M%S')
+                        ch = ''
+                        ch += 2 * ' ' + '<programme start="' + str(start) + ' '+time_zone+'" stop="' + str(end) + ' '+time_zone+'" channel="' + ch_name.replace('.','') + '">\n'
+                        ch += 4 * ' ' + '<title lang="fr">' + title.replace('&', 'and') + '</title>\n'
+                        ch += 4 * ' ' + '<desc lang="fr">' + subtitle.replace('&', 'and') + '</desc>\n  </programme>\r'
+                        with io.open(EPG_ROOT + '/caf.xml', "a", encoding='UTF-8')as f:
+                            f.write(ch)
+            except KeyError:pass
+        if ch_name and endtime:
+            print(ch_name.replace('.','')+' EPG ends at : '+str(datetime.fromtimestamp(endtime).strftime('%Y-%m-%d %H:%M')))
+            sys.stdout.flush()
         
 def main():
     print('**************Canal+AF EPG******************')
