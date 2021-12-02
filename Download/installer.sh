78e5d99102ac3f25e51bc9912bb787f7681e8f75
RAED <rrrr53@hotmail.com>
2021-12-03 01:45:51 +0300

    Bump V 20.0
    Fix white space error PY3

--- Download/installer.sh
+++ Download/installer.sh
@@ -2,8 +2,8 @@
 ##setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/ziko-ZR1/Epg-plugin/master/Download/installer.sh -O - | /bin/sh
 
 ######### Only These two lines to edit with new version ######
-version=19.9
-description=What_is_NEW:\n'Remove print codes forget it from testing'
+version=20.0
+description=What_is_NEW:\n'Fix white space PY3'
 ##############################################################
 
 TEMPATH=/tmp
