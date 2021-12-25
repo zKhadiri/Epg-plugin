e0b9ce09713ac6016bff374946c61db6a0a5df56
RAED <rrrr53@hotmail.com>
2021-12-26 02:04:19 +0300

    Bump V20.2

--- Download/installer.sh
+++ Download/installer.sh
@@ -2,7 +2,7 @@
 ##setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/ziko-ZR1/Epg-plugin/master/Download/installer.sh -O - | /bin/sh
 
 ######### Only These two lines to edit with new version ######
-version=20.1
+version=20.2
 description=What_is_NEW:\n'Update Id and Reference Channel Rotana'
 ##############################################################
 
