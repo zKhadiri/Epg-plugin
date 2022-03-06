04e71ac1e1fc8f4073a044c4728da59978544cb5
Mustapha Othman <100969567+falconHorizon52@users.noreply.github.com>
2022-03-06 08:56:53 +0200

    Update epg.sh
    Run all provider auto one by one consecutively

--- run/epg.sh
+++ run/epg.sh
@@ -4,6 +4,12 @@ echo 1 > /proc/sys/vm/drop_caches
 echo 2 > /proc/sys/vm/drop_caches
 echo 3 > /proc/sys/vm/drop_caches
 
-python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/bein.py &
+for epgpy in $(find /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers -name '*.py')
+do
+ if [[ $epgpy != *'__init__.py'* ]]; then
+ python $epgpy
+ wait
+ fi
+done
 
 exit 0
