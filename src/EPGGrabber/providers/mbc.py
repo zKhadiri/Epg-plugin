70aa9fba6f0c131cccc47f1888f49c9338d09a18
RAED <rrrr53@hotmail.com>
2023-03-07 21:43:02 +0300

    Fix error ...

--- src/EPGGrabber/providers/mbc.py
+++ src/EPGGrabber/providers/mbc.py
@@ -124,25 +124,27 @@ def noor():
             today += + timedelta(days=1)
         last_hr = h
         times.append(today + timedelta(hours=h, minutes=m))
+    try:
+    	print('Noor Dubai epg ends at {}'.format(times[-1]))
+    	for elem, next_elem, tit, descri in zip(times, times[1:] + [times[0]], title, des):
+        	ch = ''
+        	startime = datetime.strptime(str(elem), '%Y-%m-%d %H:%M:%S').strftime('%Y%m%d%H%M%S')
+        	endtime = datetime.strptime(str(next_elem), '%Y-%m-%d %H:%M:%S').strftime('%Y%m%d%H%M%S')
+        	ch += 2 * ' ' + '<programme start="' + startime + ' ' + time_zone + '" stop="' + endtime + ' ' + time_zone + '" channel="noordubai">\n'
+        	ch += 4 * ' ' + '<title lang="ar">' + tit + '</title>\n'
+        	if descri == '':
+            	#ch+=4*' '+'<desc lang="ar">يتعذر الحصول على معلومات هذا البرنامج</desc>\n  </programme>\r'
+            		if not PY3:
+                		ch += 4 * ' ' + '<desc lang="ar">·إذاعة نور دبي : افتتحت في 09/05/2006 تحت شعار اجتماعية برؤية إسلامية، وهي الإذاعة الأولى التي تصغي إلى الهموم اليومية والحياتية للمقيمين في دولة الإمارات العربية المتحدة، وتطل من خلال قناة نور دبي التلفزيونية التي انطلق بثها في 01/09/2008، لتركز على الرياضة والاقتصاد والتربية والصحة، وذلك في إطار اجتماعي وبرؤية إسلامية معتدلة.</desc>\n  </programme>\r'.decode('utf-8')
+            		else:
+                		ch += 4 * ' ' + '<desc lang="ar">·إذاعة نور دبي : افتتحت في 09/05/2006 تحت شعار اجتماعية برؤية إسلامية، وهي الإذاعة الأولى التي تصغي إلى الهموم اليومية والحياتية للمقيمين في دولة الإمارات العربية المتحدة، وتطل من خلال قناة نور دبي التلفزيونية التي انطلق بثها في 01/09/2008، لتركز على الرياضة والاقتصاد والتربية والصحة، وذلك في إطار اجتماعي وبرؤية إسلامية معتدلة.</desc>\n  </programme>\r'
+        	else:
+            		ch += 4 * ' ' + '<desc lang="ar">' + descri.strip() + '</desc>\n  </programme>\r'
+        	data.append(ch)
 
-    print('Noor Dubai epg ends at {}'.format(times[-1]))
-    for elem, next_elem, tit, descri in zip(times, times[1:] + [times[0]], title, des):
-        ch = ''
-        startime = datetime.strptime(str(elem), '%Y-%m-%d %H:%M:%S').strftime('%Y%m%d%H%M%S')
-        endtime = datetime.strptime(str(next_elem), '%Y-%m-%d %H:%M:%S').strftime('%Y%m%d%H%M%S')
-        ch += 2 * ' ' + '<programme start="' + startime + ' ' + time_zone + '" stop="' + endtime + ' ' + time_zone + '" channel="noordubai">\n'
-        ch += 4 * ' ' + '<title lang="ar">' + tit + '</title>\n'
-        if descri == '':
-            #ch+=4*' '+'<desc lang="ar">يتعذر الحصول على معلومات هذا البرنامج</desc>\n  </programme>\r'
-            if not PY3:
-                ch += 4 * ' ' + '<desc lang="ar">·إذاعة نور دبي : افتتحت في 09/05/2006 تحت شعار اجتماعية برؤية إسلامية، وهي الإذاعة الأولى التي تصغي إلى الهموم اليومية والحياتية للمقيمين في دولة الإمارات العربية المتحدة، وتطل من خلال قناة نور دبي التلفزيونية التي انطلق بثها في 01/09/2008، لتركز على الرياضة والاقتصاد والتربية والصحة، وذلك في إطار اجتماعي وبرؤية إسلامية معتدلة.</desc>\n  </programme>\r'.decode('utf-8')
-            else:
-                ch += 4 * ' ' + '<desc lang="ar">·إذاعة نور دبي : افتتحت في 09/05/2006 تحت شعار اجتماعية برؤية إسلامية، وهي الإذاعة الأولى التي تصغي إلى الهموم اليومية والحياتية للمقيمين في دولة الإمارات العربية المتحدة، وتطل من خلال قناة نور دبي التلفزيونية التي انطلق بثها في 01/09/2008، لتركز على الرياضة والاقتصاد والتربية والصحة، وذلك في إطار اجتماعي وبرؤية إسلامية معتدلة.</desc>\n  </programme>\r'
-        else:
-            ch += 4 * ' ' + '<desc lang="ar">' + descri.strip() + '</desc>\n  </programme>\r'
-        data.append(ch)
-
-    data.pop(-1)
+    	data.pop(-1)
+    except:
+    	pass
 
     for epg in data:
         with io.open(EPG_ROOT + '/mbc.xml', "a", encoding="utf-8") as f:
