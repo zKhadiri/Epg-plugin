8a848de71a447699e3f0529a0541520069dfe77b
RAED <rrrr53@hotmail.com>
2023-03-09 16:50:29 +0300

    Bump V22.3
    Fix some codes ...

--- Download/installer.sh
+++ Download/installer.sh
@@ -2,8 +2,8 @@
 ##setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/ziko-ZR1/Epg-plugin/master/Download/installer.sh -O - | /bin/sh
 
 ######### Only These two lines to edit with new version ######
-version=22.2
-description=What_is_NEW:\n'-Fix rotana cloudflare protection. And beinentC crash error'
+version=22.3
+description=What_is_NEW:\n'-Fix some codes.'
 ##############################################################
 
 TEMPATH=/tmp
