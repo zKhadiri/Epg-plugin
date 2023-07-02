ec001f348a2f62a0bbc3914defa8dfd69fcd8b1f
Redouane <redouaneelrhachi@gmail.com>
2023-07-03 00:33:05 +0200

    update added configs to epg import source update check to creat bebawy6.chanel.xml if not exist

--- Download/installer.sh
+++ Download/installer.sh
@@ -2,7 +2,7 @@
 ##setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/ziko-ZR1/Epg-plugin/master/Download/installer.sh -O - | /bin/sh
 
 ######### Only These two lines to edit with new version ######
-version=22.7
+version=22.7.1
 description=What_is_NEW:\n'-fix elcin'
 ##############################################################
 
