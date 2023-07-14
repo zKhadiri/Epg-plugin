541d4fc941e1eab0243515aa5b9e4613de7cece2
RAED <rrrr53@hotmail.com>
2023-07-15 00:52:36 +0300

    Update installer.sh

--- Download/installer.sh
+++ Download/installer.sh
@@ -2,8 +2,8 @@
 ##setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/ziko-ZR1/Epg-plugin/master/Download/installer.sh -O - | /bin/sh
 
 ######### Only These two lines to edit with new version ######
-version=22.8
-description=What_is_NEW:\n'-fix Bein and Elcinema'
+version=22.9
+description=What_is_NEW:\n'-Some fix for El Cinma'
 ##############################################################
 
 TEMPATH=/tmp
