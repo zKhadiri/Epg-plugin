021821bb515e998bec1ce179abee73f3f7e09a08
RAED <rrrr53@hotmail.com>
2024-07-25 01:44:03 +0300

    Bump V23.4
    -Some fixes and update Bein Sports by Saiedf

--- Download/installer.sh
+++ Download/installer.sh
@@ -2,8 +2,8 @@
 ##setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/ziko-ZR1/Epg-plugin/master/Download/installer.sh -O - | /bin/sh
 
 ######### Only These two lines to edit with new version ######
-version=23.3
-description=What_is_NEW:\n'-Some fixes and update'
+version=23.4
+description=What_is_NEW:\n'-Some fixes and update Bein Sports by Saiedf'
 ##############################################################
 
 TEMPATH=/tmp
