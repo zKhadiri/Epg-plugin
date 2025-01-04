44901ceeec4331a3ef46e151f0937ccb472ec4c2
RAED <rrrr53@hotmail.com>
2025-01-04 13:48:43 +0300

    Bump V24.1
    * Fixes to make Script Provider work with Dreambox by Saiedf

--- Download/installer.sh
+++ Download/installer.sh
@@ -2,8 +2,8 @@
 ##setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/ziko-ZR1/Epg-plugin/master/Download/installer.sh -O - | /bin/sh
 
 ######### Only These two lines to edit with new version ######
-version=24.0
-description=What_is_NEW:\n'-Fix some minor errors by Saiedf'
+version=24.1
+description=What_is_NEW:\n'-Fixes to make Script Provider work with Dreambox'
 ##############################################################
 
 TEMPATH=/tmp
