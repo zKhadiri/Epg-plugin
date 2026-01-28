70e57efcd3d83b56e7caeff641f50dd32030edbd
RAED <rrrr53@hotmail.com>
2026-01-28 12:56:29 +0300

    Update installer.sh

--- Download/installer.sh
+++ Download/installer.sh
@@ -2,8 +2,8 @@
 ##setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/ziko-ZR1/Epg-plugin/master/Download/installer.sh -O - | /bin/sh
 
 ######### Only These two lines to edit with new version ######
-version=24.7
-description=What_is_NEW:\n'- Some update By iet5'
+version=24.8
+description=What_is_NEW:\n'- Fixing the slow download problem of the Arabic and English cinema epg script by iet5'
 ##############################################################
 
 TEMPATH=/tmp
